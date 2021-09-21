fa40e2e92e8d01fc7919460f9bda0147f66b371f
Hanxuan Chua <Hanxuan Chua@LAPTOP-QAI2QI4H>
2021-09-21 14:10:47 +0800

    Add project files.

---
+++ FindNumbsInString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string here: ");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+
+            string[] splitStr = input.Split(' ', '.', ',');
+
+            Console.WriteLine("Number of words is " + splitStr.Length);
+            Console.WriteLine();
+
+            foreach (var s in splitStr)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
