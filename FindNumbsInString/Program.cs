c4118a5112fa331c37e889e538e9ba33128eb0f3
hxchua00 <hxchua00@gmail.com>
2021-09-21 14:25:21 +0800

    Q3 added to Q2

--- FindNumbsInString/Program.cs
+++ FindNumbsInString/Program.cs
@@ -10,11 +10,12 @@ namespace SplitString
     {
         static void Main(string[] args)
         {
+            //Q2 Start
             Console.WriteLine("Enter string here: ");
             string input = Console.ReadLine();
             Console.WriteLine();
 
-            string[] splitStr = input.Split(' ', '.', ',');
+            string[] splitStr = input.Split(' ', '.', ',','?','!');
 
             Console.WriteLine("Number of words is " + splitStr.Length);
             Console.WriteLine();
@@ -22,6 +23,31 @@ namespace SplitString
             foreach (var s in splitStr)
             {
                 Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            //Q2 End
+            //Q3 start
+            Console.WriteLine("Enter word to search: ");
+            string find = Console.ReadLine();
+            Console.WriteLine();
+            int count = 0;
+            foreach(string s in splitStr)
+            {
+                if(find == s)
+                {
+                    count++;
+                }
+            }
+
+            if(count > 0)
+            {
+                Console.WriteLine(find + " has been found " + count + " times!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(find + " cannot be found anywhere");
+                Console.WriteLine();
             }
 
             Console.ReadLine();
